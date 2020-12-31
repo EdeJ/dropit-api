cd8f64e55f227089312a81fd652165ce171ccd7b
Emiel de Jong <emieldejong@xs4all.nl>
2020-12-31 10:32:08 +0100

    first commit

---
+++ src/main/java/nl/saxofoonleren/dropitapi/DropitApiApplication.java
@@ -0,0 +1,13 @@
+package nl.saxofoonleren.dropitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/saxofoonleren/dropitapi/DropitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.saxofoonleren.dropitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
